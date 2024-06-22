78d705b7e2fade98c15e9caaf01dadbc35923eca
timurmataev <150360439+timurmataev@users.noreply.github.com>
2024-06-22 08:48:18 +0600

    Добавьте файлы проекта.

---
+++ Movie/CycleExecution/StartOfExecution.fs
@@ -0,0 +1,21 @@
+﻿module StartOfExecution
+
+[<AbstractClass>]
+type Shape(startX, startY) = class
+    let mutable x = startX
+    let mutable y = startY
+    member _.printStartPoint()= printfn $"x: {x}  y: {y}"
+ 
+    abstract Area: float with get
+end
+ 
+type Rectangle(width, height, startX, startY) = 
+    inherit Shape(startX, startY)
+    member this.Width = width
+    member this.Height = height
+    override this.Area = this.Width * this.Height
+ 
+type Circle(radius, startX, startY) = 
+    inherit Shape(startX, startY)
+    member this.Radius = radius
+    override this.Area = this.Radius * System.Math.PI
---
+++ Movie/Program.fs
@@ -0,0 +1,13 @@
+﻿open StartOfExecution
+
+[<EntryPoint>]
+let main argv =
+    printfn "func main"
+
+    let print(shape: Shape) = printfn $"Area - {shape.Area}"
+
+    let rectangle = Rectangle(10.0, 20.0, 3, 4)
+    let circle = Circle(10.0, 2, 3)
+    print(rectangle)
+    print(circle)
+    0
